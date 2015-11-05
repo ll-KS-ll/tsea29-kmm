e5b6dbe4ccebcaab1b6a016e979bda59b162de65
Lukas Lindqvist <lukas.otto.lindqvist@gmail.com>
2015-11-05 10:11:48 +0100

    Omskapat
    Nu i C

---
+++ Styrenheten/main.c
@@ -0,0 +1,18 @@
+/*
+ * Styrenheten.c
+ *
+ * Created: 2015-11-05 10:11:22
+ * Author : Lukas
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
