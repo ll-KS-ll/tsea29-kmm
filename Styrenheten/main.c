931c05114d904402faf6272d7755e826c97864f2
Lukas Lindqvist <lukas.otto.lindqvist@gmail.com>
2015-11-05 15:21:52 +0100

    Fixced commit
    Last commit is rather this commit, forgot to save...

--- Styrenheten/main.c
+++ Styrenheten/main.c
@@ -8,7 +8,11 @@
 #include <avr/io.h>
 
 void initLeftMotorPWM() {
-	DDRD |= (1<<5); // set PD5 as output
+	/*
+	* Port 5 = DirLeft, Port 6 = PWMLeft
+	* Port 3 = DirRight, Port 4 = PWMRight
+	*/ 
+	DDRD = 0x; // set PD5 as output
 
 	TCCR1A |= (1<<COM1A1)|(0<<COM1A0)|(1<<WGM02)|(1<<WGM01)|(1<<WGM00);
 
@@ -17,14 +21,14 @@ void initLeftMotorPWM() {
 
 int main(void)
 {
-	//initLeftMotorPWM();
+	initLeftMotorPWM();
     /* Replace with your application code */
 	
-	DDRD |= 0b01111000; // Set ports 3 to 7 to output without changing the rest.
+	// DDRD |= 0b01111000; // Set ports 3 to 7 to output without changing the rest.
 	
     while (1) 
     {
-		PORTD = (1<<5)|(0<<6)|(1<<3)|(1<<4); // Control left and right motor
+		// PORTD = (1<<5)|(0<<6)|(1<<3)|(0<<4); // Control left and right motor
     }
 }
 
