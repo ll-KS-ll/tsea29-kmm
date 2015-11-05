5b607c753ef96e84340be1eb21beebe59e55a2c8
Lukas Lindqvist <lukas.otto.lindqvist@gmail.com>
2015-11-05 15:21:06 +0100

    Controling motor
    You can now control the motor and the direction of each motor, PortD 3
    and 4 controls direction and speed respectivly for the left-side motors,
    PortD 5 and 6 does the same for the right-side motors.

--- Styrenheten/main.c
+++ Styrenheten/main.c
@@ -8,9 +8,11 @@
 #include <avr/io.h>
 
 void initLeftMotorPWM() {
+	DDRD |= (1<<5); // set PD5 as output
+
 	TCCR1A |= (1<<COM1A1)|(0<<COM1A0)|(1<<WGM02)|(1<<WGM01)|(1<<WGM00);
+
 	OCR1A = 0x80; // set 50% duty cycle
-	DDRD |= (1<<DDB5); // set PD5 as output
 }
 
 int main(void)
@@ -18,11 +20,11 @@ int main(void)
 	//initLeftMotorPWM();
     /* Replace with your application code */
 	
-	DDRD = DDRD | 0b01010000; // Set ports 4 & 6 to output without changing the rest.
+	DDRD |= 0b01111000; // Set ports 3 to 7 to output without changing the rest.
 	
     while (1) 
     {
-		PORTD = (0<<4)|(1<<6);
+		PORTD = (1<<5)|(0<<6)|(1<<3)|(1<<4); // Control left and right motor
     }
 }
 
