80c2a613a5fad0e77dc0c50e31c38b4b24a28776
Lukas Lindqvist <lukas.otto.lindqvist@gmail.com>
2015-11-05 18:33:33 +0100

    Driving robot
    You can now drive the robot forwards and backwards. You can rotate it
    left and right. You can also set the speed for for the motors while
    doing this or before doing it or maybe you want to stand still, then set
    to rotate and then activate motors, you can do that.

--- Styrenheten/main.c
+++ Styrenheten/main.c
@@ -1,34 +1,89 @@
 /*
- * Styrenheten.c
- *
- * Created: 2015-11-05 10:11:22
- * Author : Lukas
- */ 
+* Styrenheten.c
+*
+* Created: 2015-11-05 10:11:22
+* Author : Lukas
+*/
 
+#define F_CPU 1000000UL
 #include <avr/io.h>
+#include <util/delay.h>
 
-void initLeftMotorPWM() {
+
+void initMotorPWM() {
 	/*
-	* Port 5 = DirLeft, Port 6 = PWMLeft
+	* Port 6 = DirLeft, Port 5 = PWMLeft
 	* Port 3 = DirRight, Port 4 = PWMRight
-	*/ 
-	DDRD = 0x; // set PD5 as output
+	*/
+	
+	/************************************************************************/
+	/* Setting up Timer Control Register A/B for fast PWM			        */
+	/************************************************************************/
+	//TCCRnx, COMnx, WGn1:0
+	TCCR1A |= (1<<COM1A1)|(1<<COM1B1)|(1<<WGM10);
+	TCCR1B |= (1<<WGM12)|(0<<CS11)|(1<<CS10);
 
-	TCCR1A |= (1<<COM1A1)|(0<<COM1A0)|(1<<WGM02)|(1<<WGM01)|(1<<WGM00);
-
-	OCR1A = 0x80; // set 50% duty cycle
+	
 }
+
+void setMotorSpeed(int leftSpeed, int rightSpeed) {
+	OCR1A = leftSpeed; 
+	OCR1B = rightSpeed; 
+}
+
+void forward() {
+	PORTD = (1<<3)|(0<<6);
+}
+
+void reverse() {
+	PORTD = (0<<3)|(1<<6);
+}
+
+void rotateLeft() {
+	PORTD = (1<<3)|(1<<6);
+}
+
+void rotateRight() {
+	PORTD = (0<<3)|(0<<6);
+}
+
+
 
 int main(void)
 {
-	initLeftMotorPWM();
-    /* Replace with your application code */
+	//unsigned int counter = 0;
+	// unsigned int i;
+	// Initializes the PWM signal output for the motors.
+	initMotorPWM();
+	DDRD |= 0x78; // 0111_1000;
+
+	while (1)
+	{
+		forward();
+		setMotorSpeed(0,0);
+		_delay_ms(2000);
+		setMotorSpeed(150, 150);
+		_delay_ms(3000);
+		setMotorSpeed(0,0);
+		_delay_ms(2000);
+		reverse();
+		setMotorSpeed(150, 150);
+		_delay_ms(3000);
+		setMotorSpeed(0,0);
+		_delay_ms(2000);
+		rotateLeft();
+		setMotorSpeed(150, 150);
+		_delay_ms(3000);
+		setMotorSpeed(0,0);
+		_delay_ms(2000);
+		rotateRight();
+		setMotorSpeed(150, 150);
+		_delay_ms(3000);
+		setMotorSpeed(0,0);
+		_delay_ms(2000);
+		//counter +=5;
+		//if(counter >= 200) counter = 0;
+	}
 	
-	// DDRD |= 0b01111000; // Set ports 3 to 7 to output without changing the rest.
-	
-    while (1) 
-    {
-		// PORTD = (1<<5)|(0<<6)|(1<<3)|(0<<4); // Control left and right motor
-    }
 }
 
