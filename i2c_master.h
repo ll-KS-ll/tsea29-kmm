2086c239a0d1b533b3040525b8f853ac33bca9a0
Tratten <viktor.wallstedt@gmail.com>
2015-11-09 13:49:06 +0100

    Changed names to better fit the project

--- i2c_master.h
+++ i2c_master.h
@@ -1,11 +1,11 @@
-void TWI_start(void);
-void TWI_repeated_start(void);
-void TWI_init_master(void);
-void TWI_write_address(unsigned char);
-void TWI_read_address(unsigned char);
-void TWI_write_data(unsigned char);
-void TWI_read_data(void);
-void TWI_stop(void);
+void i2c_start(void);
+void i2c_repeated_start(void);
+void i2c_init_master(void);
+void i2c_write_address(unsigned char);
+void i2c_read_address(unsigned char);
+void i2c_write_data(unsigned char);
+void i2c_read_data(void);
+void i2c_stop(void);
 
 unsigned char address=0x20, read=1, write=0;
 unsigned char write_data=0x01, recv_data;
--- i2c_slave.h
+++ i2c_slave.h
@@ -2,10 +2,10 @@
 *
 */
 
-void TWI_init_slave(void);
-void TWI_match_read_slave(void);
-void TWI_read_slave(void);
-void TWI_match_write_slave(void);
-void TWI_write_slave(void);
+void i2c_init_slave(void);
+void i2c_match_read_slave(void);
+void i2c_read_slave(void);
+void i2c_match_write_slave(void);
+void i2c_write_slave(void);
 
 unsigned char write_data,recv_data;
