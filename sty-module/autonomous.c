58fa46f96d555f672df7455ed6be972293d9471e
Lukas Lindqvist <lukas.otto.lindqvist@gmail.com>
2015-11-17 12:01:14 +0100

    Fix merge
    Fix merge

--- sty-module/autonomous.c
+++ sty-module/autonomous.c
@@ -15,32 +15,12 @@
 static bool exploring = false;
 
 void exploreLabyrinth() {
-<<<<<<< HEAD
 	exploring = true;
-=======
-	// this should not be set here, it should be set when button is pushed on sens-module to start auto-driving
-	running = true;
->>>>>>> origin/sty-module
-	
 	/*
 		Write main loop for exploring labyrinth.
 	*/
-<<<<<<< HEAD
 	while(exploring) {
 		break; 	
-=======
-	while(running) {
-		/*	Autonomous logic.
-			1. Set current square to explored, and set which directions are walls and not walls.
-			2. Find next square to explore. 
-				a. If square in front of robot is unexplored set it as target, break;
-				b. If square to the right of robot is unexplored set as target, break;
-				c. If square to the left of robot is unexplored set as target, break;
-				
-		*/
-		
-		break;
->>>>>>> origin/sty-module
 	}
 	
 }
