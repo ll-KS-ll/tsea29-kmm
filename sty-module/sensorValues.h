0743e76236e5b302895ff7c70bd35cadd845d10d
Lukas Lindqvist <lukas.otto.lindqvist@gmail.com>
2015-11-17 11:56:11 +0100

    Fix merge
    Merge fixed

--- sty-module/sensorValues.h
+++ sty-module/sensorValues.h
@@ -11,10 +11,9 @@
 #ifndef SENSORVALUES_H_
 #define SENSORVALUES_H_
 
-<<<<<<< HEAD
+
 void updateRegisters(uint8_t dataId, uint16_t dataPacket);
-int getCurrentAngle();
-=======
+
 uint16_t getCurrentAngle();
 uint16_t getFrontDistance();
 uint16_t getFrontLeftDistance();
@@ -22,12 +21,6 @@ uint16_t getFrontRightDistance();
 uint16_t getBackLeftDistance();
 uint16_t getBackRightDistance();
 
-void setCurrentAngle(uint16_t newAngle);
-void setFrontDistance(uint16_t newDistance);
-void setFrontLeftDistance(uint16_t newDistance);
-void setFrontRightDistance(uint16_t newDistance);
-void setBackLeftDistance(uint16_t newDistance);
-void setBackRightDistance(uint16_t newDistance);
->>>>>>> origin/sty-module
+
 
 #endif /* SENSORVALUES_H_ */
