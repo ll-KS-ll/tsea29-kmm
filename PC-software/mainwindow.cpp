5f77a7350872ef8c0513c136f71641a8d61db0b1
ll-KS-ll <karlssonsehlin@gmail.com>
2015-12-05 18:47:06 +0100

    Convert IR data to cm changed to match sty-module. Some GUI enhancement.

--- PC-software/mainwindow.cpp
+++ PC-software/mainwindow.cpp
@@ -25,24 +25,47 @@ void MainWindow::statusUpdated(const QString &status)
     ui->logTextBox->append("[BT] " + status);
 }
 
+unsigned int convertFrontIrDataToCm(const quint16 &data)
+{
+    float dataf = (float)data * VOLTS_PER_UNIT;
+    dataf = 60.495F * pow(dataf, -1.1904F);
+    if(dataf < 20) dataf = 20; // min limit at 20cm
+    if(dataf > 150) dataf = 150; // max limit at 150cm
+    return dataf;
+}
+
+unsigned int convertSideIrDataToCm(const quint16 &data)
+{
+    float dataf = (float)data;
+    dataf = pow((3027.4 / dataf), 1.2134);
+    if(dataf < 10) dataf = 10; // min limit at 10cm
+    if(dataf > 80) dataf = 80; // max limit at 80cm
+    return dataf;
+}
+
 void MainWindow::updateData(const quint8 &id, const quint16 &data)
 {
     /* Data recived, update gui element. */
     switch (id) {
         case IR_FRONT:
-            ui->irFront->setText(QString::number(data));
+            if(data != 0)
+                ui->irFront->setText(QString::number(convertFrontIrDataToCm(data)) + " cm");
             break;
         case IR_LEFT_FRONT:
-            ui->irFrontLeft->setText(QString::number(6050 / data));
+            if(data != 0)
+                ui->irFrontLeft->setText(QString::number(convertSideIrDataToCm(data)) + " cm");
             break;
         case IR_RIGTH_FRONT:
-            ui->irFrontRight->setText(QString::number(6050 / data));
+            if(data != 0)
+                ui->irFrontRight->setText(QString::number(convertSideIrDataToCm(data)) + " cm");
             break;
         case IR_LEFT_BACK:
-            ui->irBackLeft->setText(QString::number(6050 / data));
+            if(data != 0)
+                ui->irBackLeft->setText(QString::number(convertSideIrDataToCm(data)) + " cm");
             break;
         case IR_RIGTH_BACK:
-            ui->irBackRight->setText(QString::number(6050 / data));
+            if(data != 0)
+                ui->irBackRight->setText(QString::number(convertSideIrDataToCm(data)) + " cm");
             break;
         case LINE_SENSOR1:
             ui->reflex1->setText(QString::number(data));
@@ -64,6 +87,15 @@ void MainWindow::updateData(const quint8 &id, const quint16 &data)
             break;
         case LINE_SENSOR7:
             ui->reflex7->setText(QString::number(data));
+            break;
+        case BUTTON_AUTO:
+
+            break;
+        case BUTTON_START:
+
+            break;
+        case GYRO:
+
             break;
         default:
             ui->logTextBox->append(QString("<span style=\"color: orange;\">Unrecognized id: [" +
--- PC-software/mainwindow.h
+++ PC-software/mainwindow.h
@@ -3,13 +3,17 @@
 
 #include <QMainWindow>
 #include "bluetoothserver.h"
+#include <math.h>
 
 /* === ID CODES === */
-#define IR_FRONT 5
+#define BUTTON_START 0
+#define BUTTON_AUTO 1
+
 #define IR_LEFT_FRONT 3
 #define IR_LEFT_BACK 4
-#define IR_RIGTH_FRONT 7
+#define IR_FRONT 5
 #define IR_RIGTH_BACK 6
+#define IR_RIGTH_FRONT 7
 #define LINE_SENSOR1 10
 #define LINE_SENSOR2 11
 #define LINE_SENSOR3 12
@@ -17,7 +21,10 @@
 #define LINE_SENSOR5 14
 #define LINE_SENSOR6 15
 #define LINE_SENSOR7 16
+#define GYRO 17
 /* ================ */
+
+#define VOLTS_PER_UNIT 0.0049
 
 namespace Ui {
 class MainWindow;
